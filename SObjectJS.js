0dd29f4d7c6d9b04afda0a7c3d9136857002c4b0
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-12 09:09:30 -0400

    Update SObjectJS.js

--- SObjectJS.js
+++ SObjectJS.js
@@ -3,8 +3,18 @@ var SObject = '("test" {"Hello", World})'
 function ReadObject(object, Success) {
   
   var error = 'An Error Has Occured';
-  var result = object.replace("("|")", "{"|"}");
-  return Success;
+         var str = "I have a Lenovo Laptop, a Honor Phone, and a Samsung Tab.";
+          var Obj = {
+            Lenovo: "Dell",
+            Honor: "OnePlus",
+            Samsung: "Lenovo"
+        };
+  
+        function GFG_Fun() {
+            down.innerHTML = str.replace(/Lenovo|Honor|Samsung/gi, function(matched){
+                return alert(Obj[matched]);
+            });
+        }
 }
 
 ReadObject(SObject, alert(result))
